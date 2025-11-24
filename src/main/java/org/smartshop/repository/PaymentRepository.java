97b2f4b3d5bd21ada550835c2b48b172c66a65c4
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-11-24 21:48:51 +0100

    implement Spring Data JPA repositories

---
+++ src/main/java/org/smartshop/repository/PaymentRepository.java
@@ -0,0 +1,9 @@
+package org.smartshop.repository;
+
+import org.smartshop.entity.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+}
