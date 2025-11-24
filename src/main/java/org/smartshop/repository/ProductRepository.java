8d5fa64818a8df45706918b1f42b3c2d8d177522
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-11-24 23:07:31 +0100

    add method to find products with pagination and delete support

--- src/main/java/org/smartshop/repository/ProductRepository.java
+++ src/main/java/org/smartshop/repository/ProductRepository.java
@@ -1,9 +1,12 @@
 package org.smartshop.repository;
 
 import org.smartshop.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    Page<Product> findByDeleted(Boolean deleted, Pageable pageable);
 }
