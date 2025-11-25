781071403eb0eb808967c6ff3b7b421563a82e2f
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-11-25 11:03:41 +0100

    add UnauthorizedException &  @ControllerAdvice to return clean JSON 401 when not authenticated

---
+++ src/main/java/org/smartshop/exception/GlobalExceptionHandler.java
@@ -0,0 +1,34 @@
+package org.smartshop.exception;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+
+    @ExceptionHandler(UnauthorizedException.class)
+    public ResponseEntity<Map<String,Object>> handleUnauthorizedException(UnauthorizedException ex, HttpServletRequest request) {
+
+        return details(HttpStatus.UNAUTHORIZED, ex.getMessage(), request);
+    }
+
+    private  ResponseEntity<Map<String,Object>> details(HttpStatus status, String message, HttpServletRequest request) {
+        Map<String,Object> response = Map.of(
+                "timestamp", LocalDateTime.now(),
+                "status", status.value(),
+                "error type", status.getReasonPhrase(),
+                "message", message,
+                "path", request.getRequestURI()
+        );
+
+        return new ResponseEntity<>(response, status);
+    }
+
+}
---
+++ src/main/java/org/smartshop/exception/UnauthorizedException.java
@@ -0,0 +1,7 @@
+package org.smartshop.exception;
+
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
