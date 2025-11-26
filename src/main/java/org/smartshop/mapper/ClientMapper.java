f1fa6d1d3267c5301df60153fff48a9fc2d78147
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-11-26 16:05:52 +0100

    add @Mapping for username & role from User entity → fix null fields in ClientDTO when response

--- src/main/java/org/smartshop/mapper/ClientMapper.java
+++ src/main/java/org/smartshop/mapper/ClientMapper.java
@@ -1,12 +1,16 @@
 package org.smartshop.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.smartshop.dto.ClientDTO;
 import org.smartshop.entity.Client;
 
 @Mapper(componentModel = "spring")
 public interface ClientMapper {
+    @Mapping(target = "username", source = "user.username")
+    @Mapping(target = "role", source = "user.role")
     ClientDTO toDTO(Client client);
 
+    @Mapping(target = "user", ignore = true)
     Client toEntity(ClientDTO clientDTO);
 }
