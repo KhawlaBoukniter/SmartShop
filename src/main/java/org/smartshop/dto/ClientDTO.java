210302d579739154a05dad5e0a9773ecd5e43964
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-11-26 09:51:24 +0100

    extend User in ClientDTO

--- src/main/java/org/smartshop/dto/ClientDTO.java
+++ src/main/java/org/smartshop/dto/ClientDTO.java
@@ -12,7 +12,7 @@ import java.math.BigDecimal;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class ClientDTO {
+public class ClientDTO extends UserDTO {
     private Long id;
     private String name;
     private String email;
