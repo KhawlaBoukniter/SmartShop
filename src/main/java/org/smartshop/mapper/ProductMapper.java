65603b047d4df867831a0320af13d2cc528fe2fc
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-11-26 11:49:37 +0100

    add explicit @Mapping for price field - fix BigDecimal mapping issue

--- src/main/java/org/smartshop/mapper/ProductMapper.java
+++ src/main/java/org/smartshop/mapper/ProductMapper.java
@@ -1,12 +1,15 @@
 package org.smartshop.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.smartshop.dto.ProductDTO;
 import org.smartshop.entity.Product;
 
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
+    @Mapping(target = "price", source = "price")
     ProductDTO toDTO(Product product);
 
+    @Mapping(target = "price", source = "price")
     Product toEntity(ProductDTO productDTO);
 }
