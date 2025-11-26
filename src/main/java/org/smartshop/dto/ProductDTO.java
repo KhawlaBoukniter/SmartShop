7249b3a0ed11a8d368ffcf340062076eef7bbe92
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-11-26 16:43:56 +0100

    add full ProductDTO Bean Validation

--- src/main/java/org/smartshop/dto/ProductDTO.java
+++ src/main/java/org/smartshop/dto/ProductDTO.java
@@ -1,9 +1,12 @@
 package org.smartshop.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.smartshop.dto.Validation.Creation;
 
 import java.math.BigDecimal;
 
@@ -12,7 +15,12 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProductDTO {
+    @NotBlank(groups = Creation.class, message = "Le nom du produit est obligatoire")
     private String name;
+
+    @NotNull(groups = Creation.class, message = "Le prix est obligatoire")
     private BigDecimal price;
+
+    @NotNull(groups = Creation.class, message = "Le stock est obligatoire")
     private Integer stock;
 }
