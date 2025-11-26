293cab1796837b5b9c3357baf84ddcaeb3bf34d2
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-11-26 11:29:41 +0100

    implement full CRUD service interface with pagination support

--- src/main/java/org/smartshop/service/ProductService.java
+++ src/main/java/org/smartshop/service/ProductService.java
@@ -1,4 +1,13 @@
 package org.smartshop.service;
 
+import org.smartshop.dto.ProductDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface ProductService {
+    ProductDTO createProduct(ProductDTO productDTO);
+    ProductDTO getProduct(Long id);
+    Page<ProductDTO> getAllProducts(Pageable pageable);
+    ProductDTO updateProduct(Long id, ProductDTO productDTO);
+    void deleteProduct(Long id);
 }
