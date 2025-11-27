604139ad22919cd0e0149ad7f27fb51fd5aae4cd
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-11-27 11:16:01 +0100

    add PromoCodeDTO with full validation

---
+++ src/main/java/org/smartshop/dto/PromoCodeDTO.java
@@ -0,0 +1,18 @@
+package org.smartshop.dto;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public class PromoCodeDTO {
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private Long id;
+
+    @NotBlank(message = "Le code promo est obligatoire")
+    @Pattern(regexp = "^PROMO-[A-Z0-9]{4}$", message = "Format invalide. Doit être au format PROMO-XXXX (ex: PROMO-AB12)")
+    private String promoCode;
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private boolean used = false;
+}
