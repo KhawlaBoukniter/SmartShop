46bd74e80caac7b864e508b89102da6fedc7e419
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-11-27 12:30:18 +0100

    implement PromoCodeService with secure PROMO-XXXX generation - random 4-char alphanumeric, uniqueness guaranteed via existsByCode()

---
+++ src/main/java/org/smartshop/service/PromoCodeService.java
@@ -0,0 +1,7 @@
+package org.smartshop.service;
+
+import org.smartshop.dto.PromoCodeDTO;
+
+public interface PromoCodeService {
+    PromoCodeDTO generateUniqueCode();
+}
---
+++ src/main/java/org/smartshop/service/PromoCodeServiceImpl.java
@@ -0,0 +1,40 @@
+package org.smartshop.service;
+
+import lombok.RequiredArgsConstructor;
+import org.smartshop.dto.PromoCodeDTO;
+import org.smartshop.entity.PromoCode;
+import org.smartshop.mapper.PromoCodeMapper;
+import org.smartshop.repository.PromoCodeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Random;
+
+@Service
+@RequiredArgsConstructor
+public class PromoCodeServiceImpl implements PromoCodeService {
+
+    private final PromoCodeRepository promoCodeRepository;
+    private final PromoCodeMapper promoCodeMapper;
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+    public PromoCodeDTO generateUniqueCode() {
+        String code;
+        do {
+            code = "PROMO-" + generateRandomCode();
+        } while (promoCodeRepository.existsByCode(code));
+
+        PromoCode promo = new PromoCode();
+        promo.setPromoCode(code);
+        promo.setUsed(false);
+        return promoCodeMapper.toDTO(promoCodeRepository.save(promo));
+    }
+
+    private String generateRandomCode() {
+        Random random = new Random();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 4; i++) {
+            sb.append(CHARACTERS.charAt(random.nextInt(CHARACTERS.length())));
+        }
+        return sb.toString();
+    }
+}
