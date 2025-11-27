ed1255c96b287695fe46b112d1eb4e9f8f5d174e
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-11-27 12:32:43 +0100

    add ADMIN endpoint /api/admin/promo-codes/generate - secure PROMO-XXXX generation via PromoCodeService

---
+++ src/main/java/org/smartshop/controller/PromoCodeController.java
@@ -0,0 +1,22 @@
+package org.smartshop.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.smartshop.dto.PromoCodeDTO;
+import org.smartshop.service.PromoCodeService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/admin/promo-codes")
+@RequiredArgsConstructor
+public class PromoCodeController {
+
+    PromoCodeService promoCodeService;
+
+    @PostMapping("/generate")
+    public ResponseEntity<PromoCodeDTO> generateOne() {
+        return ResponseEntity.ok(promoCodeService.generateUniqueCode());
+    }
+}
