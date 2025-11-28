1857064e26e04c5407901327daa504d01b718b2e
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-11-28 11:51:36 +0100

    add repository method to fetch payments by order ID ordered by number

--- src/main/java/org/smartshop/repository/PaymentRepository.java
+++ src/main/java/org/smartshop/repository/PaymentRepository.java
@@ -4,6 +4,10 @@ import org.smartshop.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+import java.util.List;
+
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    List<Payment> findByCommande_IdOrderByNumbe(Long commandeId);
 }
