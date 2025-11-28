df5803ac98020e1db365114aa4b16695cd6fda4b
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-11-28 11:18:22 +0100

    implement PaymentController

--- src/main/java/org/smartshop/controller/PaymentController.java
+++ src/main/java/org/smartshop/controller/PaymentController.java
@@ -1,5 +1,26 @@
 package org.smartshop.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.smartshop.dto.PaymentDTO;
+import org.smartshop.service.PaymentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/payments")
+@RequiredArgsConstructor
 public class PaymentController {
 
+    private final PaymentService paymentService;
+
+    @PostMapping("/order/{orderId}")
+    public ResponseEntity<PaymentDTO> addPayment(@PathVariable Long orderId, @RequestBody PaymentDTO paymentDTO) {
+        return new ResponseEntity<>(paymentService.addPayment(orderId, paymentDTO), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}/validate")
+    public ResponseEntity<PaymentDTO> validatePayment(@PathVariable Long id) {
+        return ResponseEntity.ok(paymentService.validatePayment(id));
+    }
 }
