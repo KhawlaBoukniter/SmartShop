07868275e4c7115c442e98e85da2319fd44c7113
Khawla Boukniter <boukniter.khawla@gmail.com>
2025-11-28 11:34:00 +0100

    complete PromoCodeDTO with Lombok (@Data/@Builder) + explicit id mapping in mapper

--- src/main/java/org/smartshop/dto/PromoCodeDTO.java
+++ src/main/java/org/smartshop/dto/PromoCodeDTO.java
@@ -4,7 +4,15 @@ package org.smartshop.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PromoCodeDTO {
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Long id;
--- src/main/java/org/smartshop/mapper/PromoCodeMapper.java
+++ src/main/java/org/smartshop/mapper/PromoCodeMapper.java
@@ -1,11 +1,13 @@
 package org.smartshop.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.smartshop.dto.PromoCodeDTO;
 import org.smartshop.entity.PromoCode;
 
 @Mapper(componentModel = "spring")
 public interface PromoCodeMapper {
+    @Mapping(target = "id", source = "id")
     PromoCodeDTO toDTO(PromoCode promoCode);
     PromoCode toEntity(PromoCodeDTO promoCodeDTO);
 }
