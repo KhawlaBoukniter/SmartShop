42b2247ab92a9f856f90bd32ba8e7ba6afd36a74
Khawla Boukniter <boukniter.khawla@gmail.com>
2026-01-27 12:00:00 +0100

    feat: config des routes et services

---
+++ frontend/src/routes/AppRoutes.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import LoginPage from '../pages/auth/LoginPage';
+import AdminDashboard from '../pages/admin/AdminDashboard';
+import ClientDashboard from '../pages/client/ClientDashboard';
+import UnauthorizedPage from '../pages/error/UnauthorizedPage';
+import ProtectedRoute from '../components/auth/ProtectedRoute';
+
+const AppRoutes: React.FC = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/unauthorized" element={<UnauthorizedPage />} />
+
+                <Route
+                    path="/admin"
+                    element={
+                        <ProtectedRoute allowedRoles={['ADMIN']}>
+                            <AdminDashboard />
+                        </ProtectedRoute>
+                    }
+                />
+
+                <Route
+                    path="/client"
+                    element={
+                        <ProtectedRoute allowedRoles={['CLIENT']}>
+                            <ClientDashboard />
+                        </ProtectedRoute>
+                    }
+                />
+
+                <Route path="/" element={<Navigate to="/login" replace />} />
+                <Route path="*" element={<Navigate to="/login" replace />} />
+            </Routes>
+        </BrowserRouter>
+    );
+};
+
+export default AppRoutes;
